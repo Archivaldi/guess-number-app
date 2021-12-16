bf57ed263260082b42ec0f201bd61bd8316f5bee
Archivaldi <artur.markov1860@gmail.com>
2021-12-16 12:07:29 -0600

    adding images and styles on image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,18 @@
 import React from "react";
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, Image} from 'react-native';
+
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The game is over</Text>
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>The correct number: {props.userNumber}</Text>
+            <TitleText>The game is over</TitleText>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require('../assets/Images/success.png')} />
+            </View>
+            <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+            <BodyText>The correct number: {props.userNumber}</BodyText>
             <Button title='NEW GAME' onPress={() => {props.onConfigureNewGame()}}/>
         </View>
     )
@@ -17,6 +23,18 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    }, 
+    imageContainer: {
+        borderRadius: 200,
+        borderWidth: 3,
+        borderColor: 'black',
+        width: '80%',
+        height: 300,
+        overflow: 'hidden'
+    },
+    image: {
+        width: '100%',
+        height: '100%',
     }
 });
 
